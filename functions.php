02c5458d9fa41eaf341d2e6eeb3d620294ba852a
aydevelop <ay.develop1@gmail.com>
2020-03-23 18:00:11 +0200

    Adding Scripts

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+add_action('wp_enqueue_scripts', 'fancy_scripts');
+function fancy_scripts(){
+    wp_enqueue_style('fancy-style', get_stylesheet_uri(), array(), filemtime(get_template_directory().'/style.css'), 'all');
+    
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.3.1', 'all');
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4.4.1', true);
+}
+
